58ade603d84e2fa198f06f2163e897a1812ec0a5
cailingxiao <cailingxiao2013@qq.com>
2024-12-18 23:25:53 +0800

    update 调整模型models接口输出

--- litellm/proxy/wuban/models.py
+++ litellm/proxy/wuban/models.py
@@ -9,9 +9,11 @@ from litellm.proxy._types import (
 from litellm.proxy.auth.user_api_key_auth import user_api_key_auth
 from .WLog import log
 
-TAG = "wuban models"
+TAG = "librechat"
 router = APIRouter()
 model_list: Callable
+
+inner_models = ("azureOpenAI", "openAI", "bingAI", "chatGPTBrowser", "google", "gptPlugins", "anthropic", "assistants", "azureAssistants", "agents", "bedrock")
 
 def set_model_list_def(model_list_from_proxy: Callable):
     global model_list
@@ -37,12 +39,16 @@ async def models(user_api_key_dict: UserAPIKeyAuth = Depends(user_api_key_auth))
             outData = {}
             for item in data:
                 log(TAG, item)
-                group_name = str(item['id']).split("/")[0]
+                ## 模型名字为：模型分组/模型具体名
+                name = str(item['id'])
+                index = name.index("/")
+                group_name = name[0:index]
+                model_name = name[index+1:]
                 group = outData.get(group_name)
                 if group is None:
                     group = []
                     outData[group_name] = group
-                group.append(item["id"])
+                group.append(model_name)
             print(TAG, outData)
             return outData
     return {}
@@ -64,8 +70,13 @@ async def endpoints(user_api_key_dict: UserAPIKeyAuth = Depends(user_api_key_aut
             'order': idx
         }
         idx += 1
+        if not isInnerModel(key):
+            outData[key]["type"] = "custom"
+
     return outData
 
+def isInnerModel(name):
+    return inner_models.__contains__(name)
 
 @router.get(
     "/api/keys",
